5389db7c6cb7dbadb3a0f9934e7776dc1ba28d9c
jubran nassar <jubrann@spacelift.io>
2023-08-22 15:40:25 -0400

    fixes string output

--- main.tf
+++ main.tf
@@ -10,9 +10,9 @@ resource "spacelift_context" "string-result" {
 resource "spacelift_environment_variable" "random-string-result" {
   context_id = spacelift_context.string-result.id
   name       = "random-string-result"
-  value      = random_string.string.result
+  value      = random_string.string.id
 }
 
 output "string" {
-  value = random_string.string.result
+  value = random_string.string.id
 }
